7f8e85ebe373272638911e1e8c9399fd6ba1ddb4
licano2008 <jimgala_2006@hotmail.com>
2020-03-06 06:38:39 -0500

    Login
    Login

--- app/src/main/java/com/example/myapppersonal/MainActivity.java
+++ app/src/main/java/com/example/myapppersonal/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.myapppersonal;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
